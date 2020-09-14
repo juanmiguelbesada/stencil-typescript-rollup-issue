9ec3947efa153ec7fe60a08f0f47dd7daddb26e2
Juan Miguel Besada <juanmiguelbesada@gmail.com>
2020-09-14 10:43:16 +0200

    Initial commit

---
+++ src/components/my-component/my-component.tsx
@@ -0,0 +1,36 @@
+import { Component, Prop, h } from '@stencil/core';
+import { format } from '../../utils/utils';
+import "@google-web-components/google-chart";
+
+@Component({
+  tag: 'my-component',
+  styleUrl: 'my-component.css',
+  shadow: true,
+})
+export class MyComponent {
+  /**
+   * The first name
+   */
+  @Prop() first: string;
+
+  /**
+   * The middle name
+   */
+  @Prop() middle: string;
+
+  /**
+   * The last name
+   */
+  @Prop() last: string;
+
+  private getText(): string {
+    return format(this.first, this.middle, this.last);
+  }
+
+  render() {
+    return [
+      <div>Hello, World! I'm {this.getText()}</div>,
+      <google-chart data='[["Month", "Days"], ["Jan", 31]]'></google-chart>
+    ];
+  }
+}
